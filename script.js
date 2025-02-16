454e4d5a5e1213122902a5d4d8c50d89bb1662ea
Sakib-w <muajbin354@gmail.com>
2025-02-16 21:42:58 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
     console.log("JavaScript is linked and working!");
 });
+<img src="https://raw.githubusercontent.com/Sakib-w/Sakib-s-Area/main/img_1_1739717617945.jpg" alt="Image">
